ad176a220ae4aadd3c55808a4bf9f95244a871c6
cvalentin7 <77013631+cvalentin7@users.noreply.github.com>
2022-05-11 13:28:59 +0300

    Create say.js

---
+++ commands/info/say.js
@@ -0,0 +1,29 @@
+const { Message, Client } = require("discord.js");
+const { roles } = require("../../config.json");
+
+module.exports = {
+    name: "say",
+    usage: "!say <mesajul>",
+    description: "Trimite mesajul pe acelasi canal doar ca trimis de bot",
+    category: "staff",
+    /**
+   *
+   * @param {Client} client
+   * @param {Message} message
+   */
+    run: async (client, message, args) => {
+        try {
+            let arguments = args.slice(0).join(' ');
+            if(!arguments) return message.reply("Ai uitat sa completezi boss");
+            if(message.member.roles.cache.some(r => roles.includes(r.id)) ) {
+
+            }else {
+              message.reply(`Nu ai rolul bengos`).then(msg => {setTimeout(() => msg.delete(), 30000)})
+            }
+            await message.channel.send(arguments);
+            message.delete(1000);
+        } catch (err) {
+          client.error(err);
+        }
+    },
+}
