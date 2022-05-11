684976a9bd5297d99a6fdac51a2088763ad80b4e
cvalentin7 <77013631+cvalentin7@users.noreply.github.com>
2022-05-11 08:54:38 +0300

    Update PolitieInfo1.js
    modificare variabila pentru viitoarele comenzi `polinfo2`, `polinfo3`, etc

--- commands/Politie/PolitieInfo1.js
+++ commands/Politie/PolitieInfo1.js
@@ -12,7 +12,7 @@ module.exports = {
    */
   run: async (client, message) => {
     try {
-      const embed = new MessageEmbed()
+      let embed = new MessageEmbed()
       .setTitle("Activitate")
       .addField("```Copiati si completati modelul de mai jos:```", " **La intrare pe tura completati ⏬ Model** \n ```!activitate . \n \n Ora Intrarii pe tura:   ``` \n **La iesirea de pe tura completati ⏬ Model** ```!activitate . \n \n Ora Intrarii pe tura:  \n Iesirea de pe tura:  \n Minute Totale :\n Minute de Pauza / Abateri:``` ", true)
       .setImage(encodeURI(`https://cdn.discordapp.com/attachments/955882211117269052/956984702726717530/Unbenannt-1activitate.png`))
