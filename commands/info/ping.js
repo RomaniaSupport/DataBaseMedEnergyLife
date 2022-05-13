a0ecc167627494598d5bbd582845fdab08965869
cvalentin7 <77013631+cvalentin7@users.noreply.github.com>
2022-05-13 16:31:29 +0300

    Create ping.js

---
+++ commands/info/ping.js
@@ -0,0 +1,15 @@
+const { Message, Client } = require("discord.js");
+module.exports = {
+    name: "ping",
+    category: "info",
+    description: "Get bot ping :/",
+/**
+ * 
+ * @param {Client} client 
+ * @param {Message} message 
+ */
+
+    run: (client, message) => {
+      message.reply(`**Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms.**`);
+    }
+  }
