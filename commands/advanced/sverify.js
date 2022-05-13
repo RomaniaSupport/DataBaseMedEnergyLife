a663c9a44dfe8fe09b51c1a6b7754e946fb24caf
cvalentin7 <77013631+cvalentin7@users.noreply.github.com>
2022-05-13 12:27:28 +0300

    Create sverify.js

---
+++ commands/advanced/sverify.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require('discord.js')
+const simplydjs = require('simply-djs')
+
+module.exports = {
+    name: "verify",
+    category: "verify",
+    run: async (client, message) => {
+      let embed = new MessageEmbed()
+        .setTitle("For moky")
+        .setDescription("Apasa in plm pe botoane unde vezi banana")
+        .setImage("https://cdn.discordapp.com/attachments/973931155550265366/973931180074344498/monkey-kinkytwt_1.gif")
+simplydjs.btnrole(client, message, {
+    embed: embed,
+    data: [
+      {
+        role: '974549649727815760',
+        label: 'babana',
+         color: 'SUCCESS',
+         emoji: '🍌'
+      }
+    ],
+  })
+}
+}
